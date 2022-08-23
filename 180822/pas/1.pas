3fea33edf2e552c90dd7232c0de72467151cfb89
Nguyen Thien <kakangocthien109@gmail.com>
2022-08-23 08:22:55 +0700

    Create 1.pas

---
+++ 180822/pas/1.pas
@@ -0,0 +1,10 @@
+program chanle;
+var a: integer;
+
+begin
+  writeln('Nhap so nguyen duong a: ');
+  readln(a);
+  if ((a mod 2) = 0) then writeln('a la so chan') else writeln('a la so le');
+  readln;
+
+end.
